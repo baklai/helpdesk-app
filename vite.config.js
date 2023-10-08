9e3e80d9142bb64eb27e36c2cb7d0dfb7a040ff8
Dmitrii <d.n.baklai@gmail.com>
2023-10-08 17:09:42 +0300

    fix: add pwa plugin

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), VitePWA({ injectRegister: 'auto', registerType: 'autoUpdate' }), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
