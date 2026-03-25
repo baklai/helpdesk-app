a52f432e670b37ed2427a614aa6a22f3e5e0ae93
Dmytro Baklai <d.n.baklai@gmail.com>
2026-03-25 14:04:05 +0200

    fix: спрощення компоненту

--- src/components/buttons/BtnNotifications.vue
+++ src/components/buttons/BtnNotifications.vue
@@ -47,83 +47,76 @@ const onRemoveRecord = async id => {
     <Modal ref="refModal" @close="refetch" />
 
     <Popover ref="refMenu" appendTo="body" class="w-140">
-      <div class="flex w-full flex-col">
-        <div class="flex justify-between px-2 pt-2">
-          <div class="flex items-center justify-center">
-            <Avatar class="mr-4" icon="pi pi-bell text-xl" size="large" />
-            <div>
-              <p class="line-height-2 text-lg font-bold">Сповіщення</p>
-              <p class="line-height-2 text-surface-500 text-base font-normal">
-                Системні сповіщення
-              </p>
-            </div>
-          </div>
-          <div class="flex items-center justify-center">
-            <Button
-              v-tooltip.bottom="'Створити сповіщення'"
-              icon="pi pi-plus-circle text-xl!"
-              rounded
-              severity="secondary"
-              variant="text"
-              @click="
-                () => {
-                  refMenu.hide();
-                  refModal.toggle({});
-                }
-              "
-            />
+      <div class="flex justify-between px-2 pt-2">
+        <div class="flex items-center justify-center">
+          <Avatar class="mr-4" icon="pi pi-bell text-xl" size="large" />
+          <div>
+            <p class="line-height-2 text-lg font-bold">Сповіщення</p>
+            <p class="line-height-2 text-surface-500 text-base font-normal">Системні сповіщення</p>
           </div>
         </div>
-
-        <Divider />
-
-        <DataView
-          v-if="notices"
-          class="max-h-120 overflow-auto"
-          :value="notices"
-          pt:content="flex flex-col gap-y-2"
-        >
-          <template #list="{ items }">
-            <Message
-              :severity="NOTICE_STATUS_SEVERITY[item.status] || 'secondary'"
-              v-for="(item, index) in items"
-              :key="`msg-${index}`"
-              pt:text="w-full!"
-            >
-              <div class="flex w-full flex-row justify-between gap-x-4">
-                <i class="pi pi-info-circle text-xl!"></i>
-
-                <div class="flex w-full flex-col gap-y-1">
-                  <p v-if="item.title" class="text-base font-medium uppercase">{{ item.title }}</p>
-                  <p
-                    v-if="item.message"
-                    class="text-surface-600 dark:text-surface-200 text-sm font-normal whitespace-pre-wrap"
-                  >
-                    {{ item.message }}
-                  </p>
-                  <p v-if="item.createdAt" class="text-muted-color text-xs font-normal">
-                    {{ dateTimeToStr(item.createdAt) }}
-                  </p>
-                </div>
-
-                <Button
-                  v-tooltip.bottom="'Закрити'"
-                  icon="pi pi-times"
-                  rounded
-                  size="small"
-                  :severity="NOTICE_STATUS_SEVERITY[item.status] || 'secondary'"
-                  variant="text"
-                  @click="onRemoveRecord(item.id)"
-                />
-              </div>
-            </Message>
-          </template>
-        </DataView>
-
-        <div v-else class="flex flex-col items-center justify-center p-2">
-          <p class="text-md text-muted-color font-medium">Сповіщення не знайдені</p>
+        <div class="flex items-center justify-center">
+          <Button
+            v-tooltip.bottom="'Створити сповіщення'"
+            icon="pi pi-plus-circle text-xl!"
+            rounded
+            severity="secondary"
+            variant="text"
+            @click="
+              () => {
+                refMenu.hide();
+                refModal.toggle({});
+              }
+            "
+          />
         </div>
       </div>
+
+      <Divider />
+
+      <DataView class="max-h-120 overflow-auto" :value="notices" pt:content="flex flex-col gap-y-2">
+        <template #list="{ items }">
+          <Message
+            :severity="NOTICE_STATUS_SEVERITY[item.status] || 'secondary'"
+            v-for="(item, index) in items"
+            :key="`msg-${index}`"
+            pt:text="w-full!"
+          >
+            <div class="flex w-full flex-row justify-between gap-x-4">
+              <i class="pi pi-info-circle text-xl!"></i>
+
+              <div class="flex w-full flex-col gap-y-1">
+                <p v-if="item.title" class="text-base font-medium uppercase">{{ item.title }}</p>
+                <p
+                  v-if="item.message"
+                  class="text-surface-600 dark:text-surface-200 text-sm font-normal whitespace-pre-wrap"
+                >
+                  {{ item.message }}
+                </p>
+                <p v-if="item.createdAt" class="text-muted-color text-xs font-normal">
+                  {{ dateTimeToStr(item.createdAt) }}
+                </p>
+              </div>
+
+              <Button
+                v-tooltip.bottom="'Закрити'"
+                icon="pi pi-times"
+                rounded
+                size="small"
+                :severity="NOTICE_STATUS_SEVERITY[item.status] || 'secondary'"
+                variant="text"
+                @click="onRemoveRecord(item.id)"
+              />
+            </div>
+          </Message>
+        </template>
+
+        <template #empty>
+          <div class="flex flex-col items-center justify-center p-2">
+            <p class="text-muted-color text-sm font-medium">Сповіщення не знайдені</p>
+          </div>
+        </template>
+      </DataView>
     </Popover>
 
     <OverlayBadge
